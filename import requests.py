e922ebc9bd77fd49f48c5d02cab3d9c4d4a2eeef
Jake Danuser <jakedinozzo@gmail.com>
2025-10-23 12:27:30 -0600

    Adding new data set.

--- import requests.py
+++ import requests.py
@@ -1,14 +1,42 @@
+### Get list of all IDs
+### Get top 50 list 
+### Run top 50 list to get top movers
+### Suggest buys
+
+### Module Imports
 import requests
 import json
+import csv
 
-btc = "https://api.coingecko.com/api/v3/coins/list"
+crypto_ids_list = "https://api.coingecko.com/api/v3/coins/list"
 
 headers = {
     "x-cg-demo-api-key" : "CG-sVDcZNtAoYfUVnW531RSBdeu"
 }
 
-response = requests.get(btc)
 
-json_data = response.json() 
-print(json.dumps(json_data, indent=4))
+
+def get_ids():
+    response = requests.get(crypto_ids_list)
+
+    json_data = response.json() 
+    print(json.dumps(json_data, indent=4))
+
+    output = json.loads(json_data)
+    return output
+
+ids_dict = get_ids()
+
+def get_top_50s():
+    output_list = []
+    with open('C:\\Users\\Jake\\Documents\\Python\\Fluent\\Chapter1\\top_50_crypto.csv', 'r', newline='\n') as file:
+        reader = csv.DictReader(file) 
+        for row in reader:
+            print(row)
+            output_list.append(row)
+    return output_list
+
+top50 = get_top_50s()
+
+
 
